0c5284c865f043263ee500d7825230ac46292757
Vlad Ungurean <ghiena14@gmail.com>
2023-08-24 23:39:58 +0300

    test 1.3

--- src/components/boody/aboutMe/AboutMe.jsx
+++ src/components/boody/aboutMe/AboutMe.jsx
@@ -10,6 +10,11 @@ export default function AboutMe() {
 
         <img
           className="w-[165px] h-[150px] mr-8 rounded-lg"
+          src="/public/img/fancy.jpg"
+          alt="desk"
+        />
+        <img
+          className="w-[165px] h-[150px] mr-8 rounded-lg"
           src="/src/assets/img/fancy.jpg"
           alt="desk"
         />
