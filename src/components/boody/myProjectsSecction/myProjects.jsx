df33ed9d18f4fdc194ab00fb755834ada73edf5b
Vlad Ungurean <ghiena14@gmail.com>
2023-09-09 20:52:18 +0300

    cointrack added to projects

--- src/components/boody/myProjectsSecction/myProjects.jsx
+++ src/components/boody/myProjectsSecction/myProjects.jsx
@@ -54,29 +54,33 @@ export default function MyProjects() {
           </div>
 
           <div className="flex flex-row justify-center text-center max-lg:flex-col max-lg:items-center my-8 py-3 max-lg:border-2 max-lg:border-gray-300  max-lg:bg-gray-300 max-lg:bg-opacity-50 max-lg:rounded max-lg:shadow-[10px_10px_10px_rgba(0,0,0,0.15)] ">
+            <img
+              className="w-[340px] h-[280px] mr-8 rounded-lg flex max-lg:hidden"
+              src="/img/crypto-site.png"
+              alt="desk"
+            />
+
             <div className="w-[500px] flex flex-col justify-center max-sm:w-auto max-sm:px-2 max-lg:mb-5">
-              <p className=" text-xl font-bold mb-2">USEC GYM &#127947;</p>
+              <p className=" text-xl font-bold mb-2">COINTRACK &#129689;</p>
               <p className="text-xl font-poppins">
-                A gym website is a comprehensive resource for fitness
-                information, class schedules, membership options and tools to
-                help user to achive their fitness goals.
+                COINTRACK is a crypto app that allows users to search for
+                information about various cryptocurrencies in real-time
               </p>
               <div className="flex flex-row justify-center">
-                <p className=" text-xl font-bold m-2">HTML</p>
-                <p className=" text-xl font-bold m-2">CSS</p>
-                <p className=" text-xl font-bold m-2">Java Script</p>
+                <p className=" text-xl font-bold m-2">React</p>
+                <p className=" text-xl font-bold m-2">Tailwind CSS</p>
               </div>
               <div className="flex flex-row justify-center items-center mt-2">
                 <a
                   className=" text-xl font-bold flex flex-row justify-start items-center mr-4"
-                  href="https://github.com/VladUngurean/Gym"
+                  href="https://github.com/VladUngurean/crypto-track-website"
                 >
                   <FaGithub className="text-[25px] animate-bounce mx-1" />
                   Code
                 </a>
                 <a
                   className=" text-xl font-bold flex flex-row justify-start items-center"
-                  href="https://vladungurean.github.io/Gym/"
+                  href="https://crypto-track-website.vercel.app/"
                 >
                   <FaCloudUploadAlt className="text-[25px] animate-bounce mr-1" />
                   Live Demo
@@ -85,8 +89,8 @@ export default function MyProjects() {
             </div>
 
             <img
-              className="w-[340px] h-[280px] mr-8 rounded-lg"
-              src="/img/gym-main.png"
+              className="w-[340px] h-[280px] mr-8 rounded-lg hidden max-lg:flex"
+              src="/img/crypto-site.png"
               alt="desk"
             />
           </div>
