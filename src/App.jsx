14210b33429e21939bc90336778270c66084fe9d
Vlad Ungurean <ghiena14@gmail.com>
2023-09-10 19:35:22 +0300

    code cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
-import { useState } from "react";
 import NavBar from "./components/navBar/NavBar";
 import MyIntroduction from "./components/boody/aboutMe/MyIntroduction";
-// import TechStack from "./components/boody/aboutMe/TechStack";
 import AboutMe from "./components/boody/aboutMe/AboutMe";
 import MyProjects from "./components/boody/myProjectsSecction/myProjects";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <NavBar />
       <MyIntroduction />
-      {/* <TechStack /> */}
       <AboutMe />
       <MyProjects />
     </>
