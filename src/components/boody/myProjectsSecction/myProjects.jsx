839d1da1c73ebe3edfa06c167dc8eeb63e1f562c
VladUngurean <136657499+VladUngurean@users.noreply.github.com>
2024-03-06 16:34:48 +0200

    chDream Cars project added

--- src/components/boody/myProjectsSecction/myProjects.jsx
+++ src/components/boody/myProjectsSecction/myProjects.jsx
@@ -19,6 +19,52 @@ export default function MyProjects() {
           </p>
 
           <div className="flex flex-row justify-center text-center max-lg:flex-col max-lg:items-center my-8 py-3 max-lg:border-2 max-lg:border-gray-300  max-lg:bg-gray-300 max-lg:bg-opacity-50 max-lg:rounded max-lg:shadow-[10px_10px_10px_rgba(0,0,0,0.15)] ">
+            <div className="flex justify-center items-center h-full">
+              <img
+                className="w-[340px] h-[280px] mr-8 rounded-lg flex max-lg:hidden"
+                src="/img/chDreamCar.png"
+                alt="desk"
+              />
+            </div>
+
+
+            <div className="max-w-[500px] w-full flex flex-col justify-center max-sm:w-auto max-sm:px-2 max-lg:mb-5">
+              <p className=" text-xl text-left max-lg:text-center font-bold mb-2">CHISINAU DREAM CARS &#128663;</p>
+              <div className="text-xl text-left max-lg:text-center  p-[5px] font-poppins">
+              Introducing my latest project, a dynamic car rental platform. <div className=" text-red-500 inline">Despite being a work in progress</div>, this website offers users the ability to register, browse a diverse selection of cars, and seamlessly rent their ideal vehicle. With ongoing development, expect exciting features like enhanced car management and an intuitive user interface.
+              </div>
+              <div className="flex flex-row justify-start max-lg:justify-center my-[10px] gap-5">
+                <p className="text-xl font-bold">PHP</p>
+                <p className="text-xl font-bold">SCSS</p>
+                <p className="text-xl font-bold">Java Script</p>
+              </div>
+              <div className="flex flex-row justify-start max-lg:justify-center items-center mt-2">
+                <a
+                  className=" text-xl font-bold flex flex-row justify-start items-center mr-4"
+                  href="https://github.com/VladUngurean/crypto-track-website"
+                >
+                  <FaGithub className="text-[25px] animate-bounce mx-1" />
+                  Code
+                </a>
+                <a
+                  className=" text-xl font-bold flex flex-row justify-start items-center"
+                  href="https://crypto-track-website.vercel.app/"
+                >
+                  <FaCloudUploadAlt className="text-[25px] animate-bounce mr-1" />
+                  Live Demo
+                </a>
+              </div>
+            </div>
+
+            <img
+              className="w-[340px] h-[280px] mr-8 rounded-lg hidden max-lg:flex  max-lg:mr-0"
+              src="/img/chDreamCar.png"
+              alt="desk"
+            />
+          </div>
+
+
+          <div className="flex flex-row justify-center text-center max-lg:flex-col max-lg:items-center my-8 py-3 max-lg:border-2 max-lg:border-gray-300  max-lg:bg-gray-300 max-lg:bg-opacity-50 max-lg:rounded max-lg:shadow-[10px_10px_10px_rgba(0,0,0,0.15)] ">
             <div className="max-w-[500px] w-full flex flex-col justify-center max-sm:w-auto max-sm:px-2 max-lg:mb-5">
               <p className=" text-xl text-right max-lg:text-center font-bold mb-2">USEC GYM &#127947;</p>
               <p className="text-xl text-right max-lg:text-center p-[5px] font-poppins">
@@ -97,6 +143,7 @@ export default function MyProjects() {
               alt="desk"
             />
           </div>
+
         </div>
       </div>
     </>
